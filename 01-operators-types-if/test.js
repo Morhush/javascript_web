9ca440a4548c1c922947a8cb45ac29e6d76d62f1
Morhush <sasha.morhush.z@gmail.com>
2025-03-01 14:17:04 +0200

    1

---
+++ 01-operators-types-if/test.js
@@ -0,0 +1,25 @@
+//  коментар на 1 рядок
+
+/*
+коментар
+на декілька
+рядків
+*/
+
+// змінні
+let name = 'Vany';
+let number = 423;
+let numbeR = 345;
+let fullName = 'Петро Петроенко';
+let full_name = 'Петро Петренко';
+let myList = [1,2,36,987]
+let meDic = {'name':'Давид', age:25}
+let b = true;
+
+number = number + numbeR;
+
+const NAME = 'Michael'
+
+
+
+
